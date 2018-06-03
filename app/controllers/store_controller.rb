bd3d098f4f36b1825fa5d219c3849b9f08c8e8f5
Richard Twena <richardtwena@gmail.com>
2018-06-03 19:17:57 +0100

    Moved cart to side bar, using partials

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,8 +1,9 @@
 class StoreController < ApplicationController
   include SessionCounter
+  include CurrentCart
+  before_action :set_cart, :counter
 
   def index
     @products = Product.order(:title)
-    counter
   end
 end
